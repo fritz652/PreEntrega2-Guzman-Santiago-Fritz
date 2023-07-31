90a60511974a826a0b6c4b0b6b7eef9d2a9e07a5
Fritz <fritz.developer10@gmail.com>
2023-07-31 02:23:47 -0500

    eliminando codigo comentado

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-/* const Persona = function (id, nombre,distrito, negocio, monto){
-    this.id          = id;
-    this.nombre     = nombre;
-    this.distrito   = distrito;
-    this.negocio    = negocio;
-    this.monto      = monto;
-}
-
-let persona1 = new Persona(123,"Fritz Guzmán", "Olivos", "Zapateria", 1000);
-let persona2 = new Persona(123,"Carmen Terrones", "Olivos", "Floreria", 1200);
-let persona3 = new Persona(123,"Domitila Satiago", "San Isidro", "Ferreteria", 2000);
-let persona4 = new Persona(123,"Luis Miguel", "San Martin", "Confiteria", 500);
-let persona5 = new Persona(123,"Manuel Servantes", "Lima", "Polleria", 3000);
-let persona6 = new Persona(123,"Natalia", "San Borja", "Panaderia", 2500);
-
-let cartera = [persona1, persona2, persona3, persona4, persona5, persona6];
-
-
-function filtrarPorDistrito (){
-    let palabraClave = prompt("Ingresa el distrito para buscar por distrito");
-    palabraClave = palabraClave.toLocaleLowerCase();
-    let resultado = cartera.filter((per) => per.distrito.toLowerCase().includes(palabraClave));
-
-    if(resultado.length > 0){
-        console.log(resultado);
-    }else{
-        alert(`No hay clientes en el distrito ${palabraClave}`)
-    }
-}
-
-console.log(filtrarPorDistrito()); */
-
 const Persona = function (id, nombre, distrito, negocio, monto) {
     this.id = id;
     this.nombre = nombre;
